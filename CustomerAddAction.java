d2f54f30c8b4950dc0074570619aa9b4310cd19f
imzkj <601869492@qq.com>
2017-04-21 18:51:13 +0800

    更改

--- CustomerAddAction.java
+++ CustomerAddAction.java
@@ -1,36 +1,54 @@
-package com.ibeifeng.action;
+package com.fred.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public abstract class WebAction {
+import com.fred.action.CustomerAddAction;
+import com.fred.action.WebAction;
+
+/**
+ * Servlet implementation class WebController
+ */
+@WebServlet("/action")
+public class WebController extends HttpServlet {
+	private Map map;
 	
-	/**
-	 * 执行方法
-	 */
-	public abstract void execute(HttpServlet servlet,HttpServletRequest request, HttpServletResponse response);
-	
-	/**
-	 * 跳转方法
-	 */
-	public void forward(HttpServlet servlet,HttpServletRequest request, HttpServletResponse response,String page){
-		//获得ServletContext
-		ServletContext sc = servlet.getServletContext();
-		//获得RequestDispatcher，并指定跳转页面
-		RequestDispatcher rdispatcher = sc.getRequestDispatcher(page);
-		//执行跳转
-		try {
-			rdispatcher.forward(request, response);
-		} catch (ServletException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+	public WebController() {
+		map = new HashMap();
+		map.put("CustomerAdd", CustomerAddAction.class);
+	}
+
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//接受action参数
+		String action = request.getParameter("action");
+		//判断是否为空，是否为null
+		if(action != null && !"".equals(action)) {
+			//取得相应的业务控制器类
+			Class actionClass = (Class) map.get(action);
+			//获得该类的实例，并进行强制类型转换
+			try {
+				WebAction webAction = (WebAction)(actionClass.newInstance());
+				webAction.execute(this,request,response);
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
 		}
+		
 	}
 }
