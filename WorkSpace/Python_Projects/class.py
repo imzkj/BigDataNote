cc131951b24d7d3256cc9665a1c4c60f2ba2037f
ZKJ <601869492@qq.com>
2017-10-30 08:34:53 +0800

    去公司

---
+++ WorkSpace/Python_Projects/class.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+#双下划线，目的避免子类对父类同名属性的冲突
+class Student(object):
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        self.__address='Shanghai'
+
+joke=Student('Joke',28)
+# print(joke.__address
+print(joke._Student__address)
+#类内的双下划线的内容，类外调用使用被改为属性前面加上单下划线和类名
---
+++ WorkSpace/Python_Projects/pandas/series.py
@@ -0,0 +1,3 @@
+import numpy as py
+import pandas as pd
+from pandas import Series,DataFrame
